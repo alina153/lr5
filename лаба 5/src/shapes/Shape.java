2d3a320d91bf754913fcfc5f83e67bbea0821876
alina153 <72145914+alina153@users.noreply.github.com>
2021-05-16 14:35:22 +0300

    Add files via upload

---
+++ лаба 5/src/shapes/Shape.java
@@ -0,0 +1,38 @@
+package shapes;
+
+public abstract class Shape {
+
+    protected String color;
+
+    protected Boolean filled;
+
+    public Shape() {
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+
+}
